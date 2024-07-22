47706934ba9c04b34b8eafc5cd1b7a2be8bbd332
Duque Gardeazabal <nduque@giub.local@climcal3.giub.unibe.ch>
2024-07-22 10:21:20 +0200

    Selecting the strong but from ModE-RA

--- 00_settings.R
+++ 00_settings.R
@@ -12,3 +12,10 @@ dir.data.ModEsim <- "/mnt/climstor/ERC_PALAEO/ModE-Sim/outdata/"
 
 dir.base <- "/scratch2/nduque/50_ITCZ/ITCZ_var/"
 
+
+###############################-
+# Insert here the local directory that contains the ModE-RA data
+###############################-
+
+dir.data.ModERA <- "/mnt/climstor/ERC_PALAEO/ModE-RA/outdata/"
+
