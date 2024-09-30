f3962ed51a29ce6a73018fcfe881fe4f54b91ca5
Duque Gardeazabal <nduque@giub.local@climpa33.giub.unibe.ch>
2024-09-30 11:50:22 +0200

    Addition of the ensemble mean to the members uncertainty bands. Also the filtering of the tropics was changed and this implied to change the internal function on processing each member for finding the location of the maximum strong Ascent. Change in graphs

--- 02_analysis/02_strongAscent_timeseries_members.R
+++ 02_analysis/02_strongAscent_timeseries_members.R
@@ -23,21 +23,33 @@ sets <- data.frame( Set= paste0("set_",rep(1420,3),"-",seq(1,3)) %>% rep(.,each=
                     Memb= paste0("m00",seq(1,9)) %>% c(.,paste0("m0",seq(10,60))),
                     Epoch= 1,
                     stringsAsFactors = F
-) %>% 
+                    ) %>% 
   rbind(., data.frame( Set= "set_1850-1" %>% rep(., each=20),
                        Memb= paste0("m00",seq(1,9)) %>% c(.,paste0("m0",seq(10,20))),
-                       Epoch= 2
-  )
+                       Epoch= 2)
   ) %>% 
   rbind(., data.frame( Set= "set_1850-2" %>% rep(., each=16),
                        Memb= paste0("m0",seq(21,36)),
-                       Epoch= 2
-  )
+                       Epoch= 2)
 ) %>% rbind(., data.frame( Set= "ModE-RA",
                            Memb= paste0("m0",seq(41,60)),
-                           Epoch= 2
+                           Epoch= 2)
 )
-)
+
+# names of ensembles in subdirectories
+sets.ens <- data.frame( Set= paste0("set_",rep(1420,3),"-",seq(1,3)),
+                        Memb= rep("ensmean", 3),
+                        Epoch= 1, 
+                        stringsAsFactors = F
+                        ) %>% 
+  rbind(., data.frame( Set= paste0("set_",rep(1850,2),"-",seq(1,2)),
+                       Memb= rep("ensmean", 2),
+                       Epoch= 2)
+        ) %>% 
+  rbind(., data.frame( Set= "ModE-RA",
+                       Memb= "ensmean",
+                       Epoch= 2)
+        )
 
 ### Dates ----
 Dates <- list()
@@ -50,7 +62,10 @@ seasons <- c("DJF","JJA")
 ################################-
 ## load data ----
 ################################-
-Omega <<- list()
+Omega <<- list() 
+for(i in unique(sets$Set)) Omega[[i]] <- list()
+
+Omega.ens <- list()
 
 f <- nc_open("./01_Data/02_Omega500/ModE-Sim_set_1420-1_to_3_Omega500hPa-ZonMean_1420-1849_sDJF.nc")
 lat <<- f$dim$lat$vals; lon <<- f$dim$lon$vals; lev <<- f$dim$plev$vals
@@ -59,7 +74,7 @@ lat <<- f$dim$lat$vals; lon <<- f$dim$lon$vals; lev <<- f$dim$plev$vals
 read_Mod <- function (Set, Memb, Epoch, varn="omega"){
   
   Dataset <- "ModE-Sim"
-  Omega[[Set]] <- list()
+  
   if(Epoch == 1) {t.span <- "1420-1849"} else if(Epoch == 2){ t.span <- "1850-2009"}
   
   f.mod <- paste0("./01_Data/02_Omega500/",Set,"/",Dataset,"_",Set,"_",Memb,"_Omega500hPa-ZonMean_",t.span)
@@ -74,7 +89,7 @@ read_Mod <- function (Set, Memb, Epoch, varn="omega"){
   
   Omega[[Set]][[Memb]] <<- ncvar_get(f, varid=varn)
 
-  return(Omega)
+  # return(Omega)
 }
 
 print("reading members")
@@ -87,12 +102,33 @@ for (i in 1:nrow(sets)){
 }
 close(pb)
 
+print("reading ensemble means")
+varn <- "omega"
+for (i in 1:nrow(sets.ens)){
+  
+  Dataset <- "ModE-Sim"
+  f <- with(sets.ens[i,],{
+    
+    if(Epoch == 1) {t.span <- "1420-1849"} else if(Epoch == 2){ t.span <- "1850-2009"}
+    f.mod <- paste0("./01_Data/02_Omega500/",Set,"/",Dataset,"_",Set,"_",Memb,"_Omega500hPa-ZonMean_",t.span)
+    if (Set == "ModE-RA"){
+      Dataset <- "ModE-RA"
+      t.span <- "1421-2008"
+      f.mod <- paste0("./01_Data/02_Omega500/",Dataset,"_lowres_20mem_Set_1420-3_1850-1_Omega500hPa-ZonMean_",t.span)
+    }
+    
+    paste0(f.mod,"_mon.nc") %>% nc_open(.)
+  })
+  Omega.ens[[sets.ens$Set[i]]] <- ncvar_get(f, varid=varn)
+}
+
 # save(Omega, file="./01_Data/02_Omega500/_allMemb_Omega500hPa.Rdata")
 
 ################################-
 # DJF & JJA aggregation ----
 ################################-
 Omega.s <- list()
+Omega.ens.s <- list()
 
 DJF_JJA <- function( Omega, Dates.memb){
   
@@ -118,6 +154,16 @@ for ( i in names(Omega)){
 
 }
 stopCluster(cl)
+
+print("seasonal accumulation Ensemble Means")
+for ( i in names(Omega.ens)){
+  print(i)
+  
+  Epoch <- with (sets.ens, Epoch[i==Set][1])
+  if (i=="ModE-RA") Epoch <- "ModERA" else Epoch <- paste0("ep",Epoch)
+  
+  Omega.ens.s[[i]] <- DJF_JJA(Omega.ens[[i]], Dates[[Epoch]])
+}
 # save(Omega.s, file="./01_Data/02_Omega500/_allMemb_Omega500hPa_DJFJJA.Rdata")
 
 ################################-
@@ -128,12 +174,16 @@ stopCluster(cl)
 # the function for identifying the ITCZ feature' location, based on smoothing spline, is loaded with the settings file (00_settings.R)
 
 # function for calculating the feature for both seasons with the smoothing spline
-min.max.memb <- function(y, lat, feature ="loc"){
+min.max.memb <- function(y, Lat, feature ="loc", Trop.fil){
+  
+  # filter to tropics
+  for (j in seasons) y[[j]] <- y[[j]][, Trop.fil[,j]]
+  Lat.trop.fil <- Lat[Trop.fil]
   
   Memb <- list()
   if (feature =="loc"){
-    Memb[["DJF"]] <- smt.min.max(y[["DJF"]], "min", lat)
-    Memb[["JJA"]] <- smt.min.max(y[["JJA"]], "min", lat)  
+    Memb[["DJF"]] <- smt.min.max(y[["DJF"]], "min", Lat.trop.fil)
+    Memb[["JJA"]] <- smt.min.max(y[["JJA"]], "min", Lat.trop.fil)  
   } else if(feature == "Str"){
     Memb[["DJF"]] <- apply(y[["DJF"]], 1, min)
     Memb[["JJA"]] <- apply(y[["JJA"]], 1, min)
@@ -143,8 +193,11 @@ min.max.memb <- function(y, lat, feature ="loc"){
   return(Memb)
 }
 
-loc.strAsc <- list()
-strAsc <- list()
+loc.strAsc <- list(); loc.strAsc.ens <- list()
+strAsc <- list(); strAsc.ens <- list()
+
+trop.fil <- lat >= -45 & lat <= 45
+
 Dates$ep1 <- seq(as.Date("1420-01-01"), as.Date("1849-12-01"), by="year")
 Dates$ep2 <- seq(as.Date("1850-01-01"), as.Date("2009-12-01"), by="year")
 Dates$ModERA <- seq(as.Date("1421-01-01"), as.Date("2008-12-01"), by="year")
@@ -155,8 +208,8 @@ for( i in names(Omega.s)){ # two time periods
   
   cl <- makeCluster(20)
   registerDoParallel(cl)
-  loc.strAsc[[i]] <- mclapply(Omega.s[[i]], min.max.memb, lat, "loc")
-  strAsc[[i]] <- mclapply(Omega.s[[i]], min.max.memb, lat, "Str")
+  loc.strAsc[[i]] <- mclapply(Omega.s[[i]], min.max.memb, lat, "loc", trop.fil)
+  strAsc[[i]] <- mclapply(Omega.s[[i]], min.max.memb, lat, "Str", trop.fil)
   stopCluster(cl)
   
   Epoch <- with (sets, Epoch[i==Set][1])
@@ -170,6 +223,19 @@ for( i in names(Omega.s)){ # two time periods
                             Dates[[Epoch]])
 }
 
+print("identifying position Ensemble means")
+for( i in names(Omega.ens.s)){ # two time periods
+  print(i)
+
+  Epoch <- with (sets.ens, Epoch[i==Set][1])
+  if (i=="ModE-RA") Epoch <- "ModERA" else Epoch <- paste0("ep",Epoch)  # defines the Epoch for attaching the dates to the time series of the members
+  dates <- Dates[[Epoch]]
+  
+  loc.strAsc[[i]][["ensmean"]] <- min.max.memb(Omega.ens.s[[i]], lat, "loc") %>% cbind.data.frame(dates, .)
+  strAsc[[i]][["ensmean"]] <- min.max.memb(Omega.ens.s[[i]], lat, "Str") %>% cbind.data.frame(dates, .)
+  
+}
+
 save(loc.strAsc,file="./01_Data/02_Omega500/_allMemb_LocStrAsc.Rdata")
 save(strAsc,file="./01_Data/02_Omega500/_allMemb_StrAsc.Rdata")
 
@@ -179,8 +245,10 @@ save(strAsc,file="./01_Data/02_Omega500/_allMemb_StrAsc.Rdata")
 load("./01_Data/02_Omega500/_allMemb_LocStrAsc.Rdata")
 load("./01_Data/02_Omega500/_allMemb_StrAsc.Rdata")
 
-Omega.loc.g <- loc.strAsc %>% 
-  reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","Season","value","Memb","Dataset")) %>% 
+loc.strAsc <- loc.strAsc %>% 
+  reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","Season","value","Memb","Dataset"))
+
+Omega.loc.g <- subset(loc.strAsc, Memb !="ensmean") %>% 
   dplyr::group_by(., Dataset, dates, Season) %>%
   dplyr::summarise(p5= quantile(value, probs = 0.05, na.rm = T),
                   p95=quantile(value, probs= 0.95, na.rm = T),
@@ -188,13 +256,15 @@ Omega.loc.g <- loc.strAsc %>%
                   max= max(value, na.rm = T)) %>% 
   as.data.frame() %>% within(., Dataset <- factor(Dataset, levels=unique(sets$Set)))
 
+Omega.loc.ens.g <- subset(loc.strAsc, Memb =="ensmean") %>% within(., Dataset <- factor(Dataset, levels=unique(sets$Set)))
+
 palette <- brewer.pal(9, "Set1")[-c(6:8)]
 
 # plot of LOCATION of the 90% uncertainty bands for each subset
-Omega.loc.g %>%
-  ggplot(., aes(x= dates, fill=Dataset)) +
+ggplot( ) +
   facet_wrap(. ~ Season, scales = "free_y",ncol=1)+
-  geom_ribbon(aes(ymin=p5,ymax=p95), alpha=0.3)+ scale_fill_manual(values = palette)+
+  geom_ribbon(data=Omega.loc.g, aes(x= dates, fill=Dataset,ymin=p5,ymax=p95), alpha=0.3)+ scale_fill_manual(values = palette)+
+  geom_line(data=Omega.loc.ens.g, aes(x= dates, y=value, color=Dataset))+ scale_color_manual(values = c(palette[-6],"black"))+
   scale_x_date(breaks = seq(as.Date("1450-01-01"),as.Date("2000-01-01"),by="50 years"), 
                             date_labels = "%Y", expand=c(0.01,0.01))+
   labs(title="Position Strong Ascent [Min. Omega 500 hPa] Ens. Memb.", y="Latitude [°]")+
@@ -202,8 +272,10 @@ Omega.loc.g %>%
                    panel.grid = element_line(linetype="dashed",color="lightgrey"),
                    axis.ticks.length=unit(-4, "pt"), axis.text.x = element_text(margin=margin(2,5,5,5),vjust = -1, size=12), axis.text.y = element_text(margin=margin(0,5,5,0,"pt"),size=12))
 
-Omega.str.g <- strAsc %>% 
-  reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","Season","value","Memb","Dataset")) %>% 
+strAsc <- strAsc %>% 
+  reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","Season","value","Memb","Dataset"))
+
+Omega.str.g <-  strAsc %>% 
   within(., {  
     Dataset <- factor(Dataset, levels=unique(sets$Set))
     value <- value*100
@@ -215,11 +287,16 @@ Omega.str.g <- strAsc %>%
                    max= max(value, na.rm = T)) %>% 
   as.data.frame()
 
+Omega.str.ens.g <- subset(strAsc, Memb =="ensmean") %>% within(.,{
+  value <- value*100
+  Dataset <- factor(Dataset, levels=unique(sets$Set))
+})
+
 # plot of STRENGHT of the 90% uncertainty band for each subset
-Omega.str.g %>%
-  ggplot(., aes(x= dates, fill=Dataset)) +
+ggplot() +
   facet_wrap(. ~ Season, scales = "free_y",ncol=1)+
-  geom_ribbon(aes(ymin=p5,ymax=p95), alpha=0.3)+ scale_fill_manual(values = palette)+
+  geom_ribbon(data= Omega.str.g, aes(x= dates, fill=Dataset, ymin=p5,ymax=p95), alpha=0.3)+ scale_fill_manual(values = palette)+
+  geom_line(data=Omega.str.ens.g, aes(x= dates, y=value, color=Dataset))+ scale_color_manual(values = c(palette[-6],"black"))+
   scale_x_date(breaks = seq(as.Date("1450-01-01"),as.Date("2000-01-01"),by="50 years"), 
                date_labels = "%Y", expand=c(0.01,0.01))+
   scale_y_continuous(transform = "reverse")+
