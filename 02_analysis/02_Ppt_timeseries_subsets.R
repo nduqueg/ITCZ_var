2b384d6f4f8e891f93e00ad00b9d149564030d4f
Duque Gardeazabal <nduque@giub.local@climpa33.giub.unibe.ch>
2024-10-03 13:16:25 +0200

    Analysis of ppt for the tropical bands, in deep tropics and neotropics (edge of tropics)

--- 02_analysis/02_Ppt_timeseries_subsets.R
+++ 02_analysis/02_Ppt_timeseries_subsets.R
@@ -72,30 +72,48 @@ Ppt <- lapply(Ppt, function(set.ppt) lapply(set.ppt, function(x){
 }))
 
 ################################-
-# Strong ascent ----
+# Maximum and mean zonal precipitation in the tropics ----
 ################################-
 # the function for identifying the ITCZ feature' location, based on smoothing spline, is loaded with the settings file (00_settings.R)
 
-loc.Ppt <- list()
-max.Ppt <- list()
+loc.max.Ppt <- max.Ppt <- mean.Ppt <- list()
 
 Lat.trop.fil <- lat[lat >= -25 & lat <= 25]
+
+mean.lat <- function(x, lat.f){
+  
+  total.weight <- cos(3.14159*lat.f/180) %>% sum()
+  
+  if( length(lat.f) == dim(x)[2]){
+    y <- sweep(x, 2, FUN="*", cos(3.14159*lat.f/180)) %>% # applying the weights
+      apply(.,1, sum, na.rm=T) %>% 
+      magrittr::divide_by(., total.weight ) # normalizing the weights
+    
+  } else{
+    stop("not equal latitude intervals in both dataset and latitude vector")
+  } 
+  
+  return(y)
+}
 
 print("Calculating position for subset:")
 for( i in names(Ppt)){
   print(i)
   
-  loc.Ppt[[i]] <- list()
+  loc.max.Ppt[[i]] <- list()
   max.Ppt[[i]] <- list()
+  mean.Ppt[[i]] <- list()
   
   for (j in seasons){
-    loc.Ppt[[i]][[j]] <- smt.min.max(Ppt[[i]][[j]][ lat >= -25 & lat <= 25, ] %>% t(),"max", Lat.trop.fil)
+    loc.max.Ppt[[i]][[j]] <- smt.min.max(Ppt[[i]][[j]][ lat >= -25 & lat <= 25, ] %>% t(),"max", Lat.trop.fil)
     max.Ppt[[i]][[j]] <- apply(Ppt[[i]][[j]][lat >= -25 & lat <= 25,], 2, max)
+    mean.Ppt[[i]][[j]] <- mean.lat(Ppt[[i]][[j]][lat >= -25 & lat <= 25,] %>% t(), Lat.trop.fil)
   } 
   
   if (substr(i, 5,8) =="1420"){ epoch <- "ep1" } else if(substr(i, 5,8) =="1850"){ epoch <- "ep2"} else { epoch <- "ModERA" }
-  loc.Ppt[[i]] <- lapply(loc.Ppt[[i]], function(x,dates) cbind.data.frame(x,dates), Dates[[epoch]])
+  loc.max.Ppt[[i]] <- lapply(loc.max.Ppt[[i]], function(x,dates) cbind.data.frame(x,dates), Dates[[epoch]])
   max.Ppt[[i]] <- lapply(max.Ppt[[i]], function(x,dates) cbind.data.frame(x,dates), Dates[[epoch]])
+  mean.Ppt[[i]] <- lapply(mean.Ppt[[i]], function(x,dates) cbind.data.frame(x,dates), Dates[[epoch]])
 }
 
 ################################-
@@ -103,7 +121,7 @@ for( i in names(Ppt)){
 ################################-
 
 # location
-Ppt.loc.g <- loc.Ppt %>% reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","variable","value","Season","Dataset")) %>% 
+Ppt.loc.g <- loc.max.Ppt %>% reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","variable","value","Season","Dataset")) %>% 
   within(., Dataset <- factor(Dataset, levels=names(Ppt))  )
 
 palette <- c("#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","black")
@@ -130,8 +148,125 @@ Ppt.str.g %>%
   geom_line()+ scale_color_manual(values = palette)+
   scale_x_date(breaks = seq(as.Date("1450-01-01"),as.Date("2000-01-01"),by="50 years"), 
                date_labels = "%Y", expand=c(0.01,0.01))+
-  labs(title="Maximum Precipitation - Subsets Ens. ", y="Ppt  [mm/month]")+
+  labs(title="Maximum zonal Precipitation - Subsets Ens. ", y="Ppt  [mm/month]")+
   theme_bw()+theme(legend.position = c(0.2,0.05), legend.direction = "horizontal",
                    panel.grid = element_line(linetype="dashed",color="lightgrey"),
+                   strip.text = element_text(size=12),
                    axis.ticks.length=unit(-4, "pt"), axis.text.x = element_text(margin=margin(2,5,5,5),vjust = -1, size=12), axis.text.y = element_text(margin=margin(0,5,5,0,"pt"),size=12))
 
+# mean Ppt
+Ppt.mean.g <- mean.Ppt %>% reshape::melt(., id=c("dates")) %>% magrittr::set_colnames(., c("dates","variable","value","Season","Dataset")) %>%
+  within(.,{
+    Dataset <- factor(Dataset, levels=names(Ppt))
+  })
+
+Ppt.mean.g %>%
+  ggplot(., aes(x= dates, y= value, col=Dataset)) +
+  facet_wrap(. ~ Season, scales = "free_y",ncol=1)+
+  geom_line()+ scale_color_manual(values = palette)+
+  scale_x_date(breaks = seq(as.Date("1450-01-01"),as.Date("2000-01-01"),by="50 years"), 
+               date_labels = "%Y", expand=c(0.01,0.01))+
+  labs(title="Mean zonal tropical Precipitation [25°S , 25°N] - Subsets Ens. ", y="Ppt  [mm/month]")+
+  theme_bw()+theme(legend.position = c(0.2,0.05), legend.direction = "horizontal",
+                   panel.grid = element_line(linetype="dashed",color="lightgrey"),
+                   strip.text = element_text(size=12),
+                   axis.ticks.length=unit(-4, "pt"), axis.text.x = element_text(margin=margin(2,5,5,5),vjust = -1, size=12), axis.text.y = element_text(margin=margin(0,5,5,0,"pt"),size=12))
+
+
+################################-
+# mean precipitation in tropical bands ----
+################################-
+# the function for identifying the ITCZ feature' location, based on smoothing spline, is loaded with the settings file (00_settings.R)
+
+trop.Ppt <- list()
+
+trop.fil <- data.frame(north= lat >= 13 & lat <= 25,
+                       deep= lat >= -13 & lat <= 13,
+                       south= lat >= -25 & lat <= -13)
+
+print("Calculating mean Ppt in tropical bands:")
+for( i in c("north","deep","south" ) ){
+  print(i)
+  
+  trop.Ppt[[i]] <- list()
+  
+  for (j in names(Ppt)){
+    
+    trop.Ppt[[i]][[j]] <- lapply( Ppt[[j]], # apply over each season
+                                  function(x, Lat.trop.fil){
+                                    z <- mean.lat(x[ trop.fil[,i], ] %>% t(), Lat.trop.fil)
+                                    return(z)
+                                    },
+                                  lat[trop.fil[,i]])
+    
+    if (substr(j, 5,8) =="1420"){ epoch <- "ep1" } else if(substr(j, 5,8) =="1850"){ epoch <- "ep2"} else { epoch <- "ModERA" }
+    
+    trop.Ppt[[i]][[j]] <- lapply(trop.Ppt[[i]][[j]], # apply over each season
+                                 function(x, dates){
+                                   z <- cbind.data.frame(x, dates); return(z)
+                                 } , Dates[[epoch]])
+  }
+}
+
+################################-
+# plot mean precipitation in tropical bands ----
+################################-
+trop.Ppt.g <- trop.Ppt %>% 
+  reshape::melt(., id="dates") %>% magrittr::set_colnames(., c("dates","variable","value","Season","Dataset", "Band")) %>% 
+  subset(., select= - variable) %>% 
+  within(., {
+    Dataset <- factor(Dataset, levels=names(Ppt))
+    Band <- factor(Band, levels= c("north","deep","south" ))
+    }) 
+
+Band.labs <- c("North [13°N,25°N]", "Deep trop. [13°S,13°N]", "South [25°S,13°S]"); names(Band.labs) <- c("north","deep","south")
+
+trop.Ppt.g %>% 
+  ggplot(., aes(x= dates, y= value, col=Dataset)) +
+  facet_grid(Band ~ Season, scales = "free_y", switch = "y", labeller= labeller(Band = Band.labs))+
+  geom_line()+ scale_color_manual(values = palette)+
+  scale_x_date(breaks = seq(as.Date("1450-01-01"),as.Date("2000-01-01"),by="50 years"), 
+               date_labels = "%Y", expand=c(0.01,0.01))+
+  labs(title="Mean zonal tropical Precipitation in bands - Subsets Ens. ", y="Ppt  [mm/month]")+
+  theme_bw()+theme(legend.position = c(0.2,0.05), legend.direction = "horizontal",
+                   panel.grid = element_line(linetype="dashed",color="lightgrey"),
+                   strip.text = element_text(size=12),
+                   axis.ticks.length=unit(-4, "pt"), axis.text.x = element_text(margin=margin(2,5,5,5),vjust = -1, size=12), axis.text.y = element_text(margin=margin(0,5,5,0,"pt"),size=12))
+
+################################-
+# anomalies precipitation in tropical bands ----
+################################-
+
+Data.anom <- list()
+anom.1500.1850 <- function(x){
+  y <- read.zoo(x, index.column = "dates")
+  
+  if (x$dates[1] < "1850-01-01") p.anom <- seq(as.Date("1500-01-01"), as.Date("1850-12-01"), by="year") else p.anom <- seq(as.Date("1850-01-01"), as.Date("1900-12-01"), by="year")
+  
+  anom <- (y - mean( y[p.anom] )) %>% 
+    fortify.zoo()
+  return(anom)
+}
+
+for( Series in c("north", "deep", "south")){
+  Data.anom[[Series]] <- trop.Ppt[[Series]] %>% 
+    lapply(., function(x) lapply(x,anom.1500.1850) ) %>% 
+    lapply(., function(x) lapply(x,`colnames<-`,c("dates","value")))
+}
+
+Data.anom %>% 
+  reshape::melt(., id=c("dates","value")) %>% magrittr::set_colnames(., c("dates","value","Season","Dataset", "Band")) %>% 
+  within(., {
+    Dataset <- factor(Dataset, levels=names(Ppt)) 
+    Band <- factor(Band, levels= c("north","deep","south" ))
+    }) %>% 
+  ggplot(., aes(x= dates, y= value, col=Dataset)) +
+  facet_grid(Band ~ Season, scales = "free_y", switch = "y", labeller= labeller(Band = Band.labs))+
+  geom_line()+ scale_color_manual(values = palette)+
+  scale_x_date(breaks = seq(as.Date("1450-01-01"),as.Date("2000-01-01"),by="50 years"), 
+               date_labels = "%Y", expand=c(0.01,0.01))+
+  labs(title="Anomalies zonal tropical Precipitation in bands - Subsets Ens. ", y="Ppt  [mm/month]")+
+  theme_bw()+theme(legend.position = c(0.7,0.05), legend.direction = "horizontal",
+                   panel.grid = element_line(linetype="dashed",color="lightgrey"),
+                   strip.text = element_text(size=12),
+                   axis.ticks.length=unit(-4, "pt"), axis.text.x = element_text(margin=margin(2,5,5,5),vjust = -1, size=12), axis.text.y = element_text(margin=margin(0,5,5,0,"pt"),size=12))
